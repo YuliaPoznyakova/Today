f467305feb9122773223eeda5bd04e590c50f30f
Yulia Poznyakova <yulia.poznyakova.bjj@gmail.com>
2024-05-28 21:36:21 +0300

    Lesson "using content views"

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Yulia Poznyakova on 28.05.2024.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}

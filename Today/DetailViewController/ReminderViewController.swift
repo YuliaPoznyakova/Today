bccf114fbe58f1d731d90fe47b0c8dc75eb2b0d8
Yulia Poznyakova <yulia.poznyakova.bjj@gmail.com>
2024-05-31 17:15:42 +0300

    Adding and removing reminders added.

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -17,6 +17,7 @@ class ReminderViewController: UICollectionViewController {
         }
     }
     var workingReminder: Reminder
+    var isAddingNewReminder = false
     var onChange: (Reminder) -> Void
     private var dataSource: DataSource!
     
@@ -30,7 +31,7 @@ class ReminderViewController: UICollectionViewController {
         let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
         super.init(collectionViewLayout: listLayout)
     }
-        
+    
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
     }
@@ -57,70 +58,75 @@ class ReminderViewController: UICollectionViewController {
         if editing {
             prepareForEditing()
         } else {
-            prepareForViewing()
+            if isAddingNewReminder {
+                onChange(workingReminder)
+            } else {
+                prepareForViewing()
+            }
         }
     }
-    
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
-        let section = section(for: indexPath)
-        switch (section, row) {
-        case (_, .header(let title)):
-            cell.contentConfiguration = headerConfiguration(for: cell, with: title)
-        case (.view, _):
-            cell.contentConfiguration = defaultConfiguration(for: cell, at: row)
-        case (.title, .editableText(let title)):
-            cell.contentConfiguration = titleConfiguration(for: cell, with: title)
-        case (.date, .editableDate(let date)):
-            cell.contentConfiguration = dateConfiguration(for: cell, with: date)
-        case (.notes, .editableText(let notes)):
-            cell.contentConfiguration = notesConfiguration(for: cell, with: notes)
-        default:
-            fatalError("Unexpected combination of section and row.")
+        
+        func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+            let section = section(for: indexPath)
+            switch (section, row) {
+            case (_, .header(let title)):
+                cell.contentConfiguration = headerConfiguration(for: cell, with: title)
+            case (.view, _):
+                cell.contentConfiguration = defaultConfiguration(for: cell, at: row)
+            case (.title, .editableText(let title)):
+                cell.contentConfiguration = titleConfiguration(for: cell, with: title)
+            case (.date, .editableDate(let date)):
+                cell.contentConfiguration = dateConfiguration(for: cell, with: date)
+            case (.notes, .editableText(let notes)):
+                cell.contentConfiguration = notesConfiguration(for: cell, with: notes)
+            default:
+                fatalError("Unexpected combination of section and row.")
+            }
+        }
+        
+        @objc func didCancelEdit() {
+            workingReminder = reminder
+            setEditing(false, animated: true)
+        }
+        
+        private func prepareForEditing() {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(
+                barButtonSystemItem: .cancel, target: self, action: #selector(didCancelEdit))
+            updateSnapshotForEditing()
+        }
+        
+        private func prepareForViewing() {
+            navigationItem.leftBarButtonItem = nil
+            if workingReminder != reminder {
+                reminder = workingReminder
+            }
+            updateSnapshotForViewing()
+        }
+        
+        private func updateSnapshotForEditing() {
+            var snapshot = Snapshot()
+            snapshot.appendSections([.title, .date, .notes])
+            snapshot.appendItems([.header(Section.title.name), .editableText(reminder.title)], toSection: .title)
+            snapshot.appendItems([.header(Section.date.name), .editableDate(reminder.dueDate)], toSection: .date)
+            snapshot.appendItems([.header(Section.notes.name), .editableText(reminder.notes)], toSection: .notes)
+            dataSource.apply(snapshot)
+        }
+        
+        private func updateSnapshotForViewing() {
+            var snapshot = Snapshot()
+            snapshot.appendSections([.view])
+            snapshot.appendItems(
+                [Row.header(""), Row.title, Row.date, Row.time, Row.notes], toSection: .view)
+            snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: .view)
+            dataSource.apply(snapshot)
+        }
+        
+        private func section(for indexPath: IndexPath) -> Section {
+            let sectionNumber = isEditing ? indexPath.section + 1 : indexPath.section
+            guard let section = Section(rawValue: sectionNumber) else {
+                fatalError("Unable to find matching section")
+            }
+            return section
         }
     }
-    
-    @objc func didCancelEdit() {
-        workingReminder = reminder
-        setEditing(false, animated: true)
-    }
-    
-    private func prepareForEditing() {
-        navigationItem.leftBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .cancel, target: self, action: #selector(didCancelEdit))
-        updateSnapshotForEditing()
-    }
-    
-    private func prepareForViewing() {
-        navigationItem.leftBarButtonItem = nil
-        if workingReminder != reminder {
-            reminder = workingReminder
-        }
-        updateSnapshotForViewing()
-    }
-    
-    private func updateSnapshotForEditing() {
-        var snapshot = Snapshot()
-        snapshot.appendSections([.title, .date, .notes])
-        snapshot.appendItems([.header(Section.title.name), .editableText(reminder.title)], toSection: .title)
-        snapshot.appendItems([.header(Section.date.name), .editableDate(reminder.dueDate)], toSection: .date)
-        snapshot.appendItems([.header(Section.notes.name), .editableText(reminder.notes)], toSection: .notes)
-        dataSource.apply(snapshot)
-    }
-    
-    private func updateSnapshotForViewing() {
-        var snapshot = Snapshot()
-        snapshot.appendSections([.view])
-        snapshot.appendItems(
-            [Row.header(""), Row.title, Row.date, Row.time, Row.notes], toSection: .view)
-        snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: .view)
-        dataSource.apply(snapshot)
-    }
-    
-    private func section(for indexPath: IndexPath) -> Section {
-        let sectionNumber = isEditing ? indexPath.section + 1 : indexPath.section
-        guard let section = Section(rawValue: sectionNumber) else {
-            fatalError("Unable to find matching section")
-        }
-        return section
-    }
-}
+
